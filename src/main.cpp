dc52cca345492df10a064f236673f88f4eccef73
HanwenLei <1191862386@qq.com>
2019-06-18 20:02:34 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@ int main(void)
 //	clock_t start = clock();
 	MyAllocate();
 //	clock_t finish = clock();
-//	getchar();
 //	double DuringTime = (finish - start) / CLOCKS_PER_SEC;
 //	printf("DuringTime : %lf seconds\n", DuringTime);
 	getchar();
